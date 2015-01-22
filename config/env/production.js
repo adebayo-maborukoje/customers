c50b6f8dbc5d7128b8d1387b01e12c0fb9ae0d16
andela-amaborukoje <adebayo@andela.co>
2015-01-22 14:23:38 +0100

    added mongolab db

--- config/env/production.js
+++ config/env/production.js
@@ -1,8 +1,9 @@
 'use strict';
-process.env.MONGOLAB_URI = 'mongodb://developer:developer@ds033601.mongolab.com:33601/developershubdb';
+
+// process.env.MONGOLAB_URI = 'mongodb://developer:developer@ds033601.mongolab.com:33601/developershubdb';
 module.exports = {
 	db: {
-		// uri: process.env.MONGOHQ_URL || || 'mongodb://developer:developer@ds033601.mongolab.com:33601/developershubdb',
+
 		 uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
 		
 		options: {
@@ -12,7 +13,7 @@ module.exports = {
 	},
 	log: {
 		// Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
-		format: 'combined',
+		format: 'dev',
 		// Stream defaults to process.stdout
 		// Uncomment to enable logging to a log on the file system
 		options: {
